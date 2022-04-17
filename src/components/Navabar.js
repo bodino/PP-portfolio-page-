644cb7d701694b130f3079e7383a747100da682d
alessandro Bifulco <a.bifulco23@gmail.com>
2022-04-17 00:14:58 -0700

    moved navbar to its proper place

--- src/components/Navabar.js
+++ src/components/Navabar.js
@@ -1,20 +1,75 @@
 import { useState } from 'react';
 import React from 'react';
-import './App.css';
-import { TwitterLogoIcon, ImageIcon, SunIcon } from '@radix-ui/react-icons'
-import {styled, darkTheme, createGlobalStyle} from './ui/stitches.config';
-import {Text, Button} from './ui/text.js';
+import {styled, darkTheme, createGlobalStyle} from '../ui/stitches.config';
+import {Text, Button} from '../ui/text.js';
+import { Example } from '../components/Example'
+import { ThemeProvider } from 'next-themes';
+import { Flexbox1, IconBoxes, NavabarContainer } from '../ui/flexboxes'
+import * as SeparatorPrimitive from '@radix-ui/react-separator';
+import { ThemeToggle } from '../ThemeToggle'
 
-import { ThemeProvider } from 'next-themes'
 
+import {
+    TwitterLogoIcon,
+    ImageIcon,
+    SunIcon,
+    HeartFilledIcon,
+    Pencil1Icon,
+    GitHubLogoIcon,
+    LinkBreak2Icon,
+    HomeIcon,
+    FaceIcon,
+  } from '@radix-ui/react-icons';
 
+  const StyledSeparator = styled(SeparatorPrimitive.Root, {
+    backgroundColor: "$buttoncolor",
+    '&[data-orientation=horizontal]': { height: 1, width: '50%' },
+    '&[data-orientation=vertical]': { height: '50%', width: 1 },
+    alignSelf: 'center',
+  });
+  
+  
+  
+  // Exports
+  export const Separator = StyledSeparator;
 
 function Navabar() {
 
   return (
-    <div>
+    <NavabarContainer>
+    <Flexbox1>
+      <IconBoxes>
+        <HomeIcon />
+      </IconBoxes>
 
-    </div>
+      <IconBoxes>
+        <Pencil1Icon />
+      </IconBoxes>
+      <Separator decorative orientation="vertical"css={{ margin: '0 15px' }}  />
+      <IconBoxes>
+        <GitHubLogoIcon />
+      </IconBoxes>
+    
+      <IconBoxes>
+        <TwitterLogoIcon />
+      </IconBoxes>
+   
+
+     
+    </Flexbox1>
+
+    <Flexbox1>
+    <IconBoxes>
+        <ThemeToggle />
+      </IconBoxes>
+
+    
+      <IconBoxes>
+        <Example/>
+      </IconBoxes>
+
+    </Flexbox1>
+   </NavabarContainer>
   );
 }
 export default Navabar;
--- src/pages/Landingpage.js
+++ src/pages/Landingpage.js
@@ -2,17 +2,7 @@ import { useState } from 'react'
 import React from 'react'
 import { useAccount } from 'wagmi'
 import '../App.css'
-import {
-  TwitterLogoIcon,
-  ImageIcon,
-  SunIcon,
-  HeartFilledIcon,
-  Pencil1Icon,
-  GitHubLogoIcon,
-  LinkBreak2Icon,
-  HomeIcon,
-  FaceIcon,
-} from '@radix-ui/react-icons'
+
 import { styled, darkTheme, createGlobalStyle } from '../ui/stitches.config'
 import { Text, Button } from '../ui/text.js'
 import { ThemeToggle } from '../ThemeToggle'
@@ -25,59 +15,19 @@ import { ThemeProvider } from 'next-themes'
 import { violet } from '@radix-ui/colors';
 import { Example } from '../components/Example'
 import Connected from '../components/Connected'
-
-
-const StyledSeparator = styled(SeparatorPrimitive.Root, {
-  backgroundColor: "$buttoncolor",
-  '&[data-orientation=horizontal]': { height: 1, width: '50%' },
-  '&[data-orientation=vertical]': { height: '50%', width: 1 },
-  alignSelf: 'center',
-});
+import Navabar from '../components/Navabar'
 
 
 
-// Exports
-export const Separator = StyledSeparator;
+
+
 
 
 function Landingpage() {
 
 
   return (
-    <NavabarContainer>
-    <Flexbox1>
-      <IconBoxes>
-        <HomeIcon />
-      </IconBoxes>
-
-      <IconBoxes>
-        <Pencil1Icon />
-      </IconBoxes>
-      <Separator decorative orientation="vertical"css={{ margin: '0 15px' }}  />
-      <IconBoxes>
-        <GitHubLogoIcon />
-      </IconBoxes>
-    
-      <IconBoxes>
-        <TwitterLogoIcon />
-      </IconBoxes>
-   
-
-     
-    </Flexbox1>
-
-    <Flexbox1>
-    <IconBoxes>
-        <ThemeToggle />
-      </IconBoxes>
-
-    
-      <IconBoxes>
-        <Example/>
-      </IconBoxes>
-
-    </Flexbox1>
-   </NavabarContainer>
+   <Navabar/>
   )
 }
 export default Landingpage
